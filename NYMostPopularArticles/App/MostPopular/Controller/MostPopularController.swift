3617e989f0ef1015a5b989af423ea807d7eacbc0
Mahmoud <mahmoud_gamal_abolfotoh@hotmail.com>
2020-12-04 18:52:37 +0200

    Add MostPopularController ui
    *Add articles tableView
    
    *Add article cell
    
    *Populate articles tableView with dummy article cells

--- NYMostPopularArticles/App/MostPopular/Controller/MostPopularController.swift
+++ NYMostPopularArticles/App/MostPopular/Controller/MostPopularController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 class MostPopularController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupTableView()
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "ArticleCell", bundle: nil), forCellReuseIdentifier: "ArticleCell")
+    }
+}
+
+extension MostPopularController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let articleCell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell") as! ArticleCell
+        return articleCell
     }
 }
---
+++ NYMostPopularArticles/App/MostPopular/View/Cell/ArticleCell.swift
@@ -0,0 +1,19 @@
+//
+//  ArticleCell.swift
+//  NYMostPopularArticles
+//
+//  Created by Mahmoud Abolfotoh on 04/12/2020.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+    @IBOutlet weak var articleTitleLabel: UILabel!
+    @IBOutlet weak var byLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
